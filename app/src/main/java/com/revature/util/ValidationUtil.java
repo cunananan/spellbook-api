6596b525de0ced1ff5be2906bd0ad4b40577ebb8
Jazlyn Maxwell <jazlyn.maxwell@gmail.com>
2022-04-13 15:58:21 -0600

    added password regex

--- app/src/main/java/com/revature/util/ValidationUtil.java
+++ app/src/main/java/com/revature/util/ValidationUtil.java
@@ -4,16 +4,17 @@ import java.util.regex.Pattern;
 
 public class ValidationUtil {
 	
-	/*
-	 * Username consists of alphanumeric characters and dots, underscores, 
+	/* Username consists of alphanumeric characters and dots, underscores, 
 	 * and hyphens and must be between 5 and 255 characters in length. 
 	 */
-	private static final String USERNAME_PATTERN = "([a-zA-Z0-9]|[._-]){5,255}$";
-	/*
-	 * Regex provided by RFC 5322 
+	private static final String USERNAME_PATTERN = "^([a-zA-Z0-9]|[._-]){5,255}$";
+	/* Email regex provided by RFC 5322 
 	 */
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-//	private static final String PASSWORD_PATTERN;
+	/* Password must have a digit [0-9], lowercase letter [a-z], uppercase letter [A-Z], 
+	 * special character, and have more than 8 characters
+	 */
+	private static final String PASSWORD_PATTERN = "^(?=.*[0-9!@#&()-[{}]:;',?/*~$^+=<>])(?=.*[a-zA-Z]).{8,255}$";
 	
 	public static boolean validateUsername(String value) {
 		return Pattern.compile(USERNAME_PATTERN).matcher(value).matches();
@@ -24,6 +25,6 @@ public class ValidationUtil {
 	}
 	
 	public static boolean validatePassword(String value) {
-		return Pattern.compile(USERNAME_PATTERN).matcher(value).matches();
+		return Pattern.compile(PASSWORD_PATTERN).matcher(value).matches();
 	}
 }
