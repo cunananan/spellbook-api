b9802c443698aded2bdf82ea9d0e84f0d5e7fdcc
Jazlyn Maxwell <jazlyn.maxwell@gmail.com>
2022-04-13 15:56:46 -0600

    added regex for validation

--- app/src/main/java/com/revature/util/ValidationUtil.java
+++ app/src/main/java/com/revature/util/ValidationUtil.java
@@ -1,21 +1,29 @@
 package com.revature.util;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.regex.Pattern;
 
 public class ValidationUtil {
 	
+	/*
+	 * Username consists of alphanumeric characters and dots, underscores, 
+	 * and hyphens and must be between 5 and 255 characters in length. 
+	 */
+	private static final String USERNAME_PATTERN = "([a-zA-Z0-9]|[._-]){5,255}$";
+	/*
+	 * Regex provided by RFC 5322 
+	 */
+	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+//	private static final String PASSWORD_PATTERN;
+	
 	public static boolean validateUsername(String value) {
-		// TODO real validation
-		return !StringUtils.isBlank(value);
+		return Pattern.compile(USERNAME_PATTERN).matcher(value).matches();
 	}
 	
 	public static boolean validateEmail(String value) {
-		// TODO real validation
-		return !StringUtils.isBlank(value);
+		return Pattern.compile(EMAIL_PATTERN).matcher(value).matches();
 	}
 	
 	public static boolean validatePassword(String value) {
-		// TODO real validation
-		return !StringUtils.isBlank(value);
+		return Pattern.compile(USERNAME_PATTERN).matcher(value).matches();
 	}
 }
