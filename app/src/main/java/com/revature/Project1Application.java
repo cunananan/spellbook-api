b02e46de4c277141fe47812243f868aa33707b6a
Jazlyn McKenzie Maxwell <jazlyn.maxwell@gmail.com>
2022-04-21 15:08:26 -0600

    added timers to service methods.

--- app/src/main/java/com/revature/Project1Application.java
+++ app/src/main/java/com/revature/Project1Application.java
@@ -2,12 +2,23 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@Configuration
 @SpringBootApplication
 public class Project1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
+	}
+	
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+	    return new TimedAspect(registry);
 	}
 
 }
