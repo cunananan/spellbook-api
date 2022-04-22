84cc80e327cceb354d8b3ed340bf2b89f8dc62c4
cunananan <cunananan@gmail.com>
2022-04-22 12:59:52 -0500

    Editing sample data

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO users (username, email, password, role)
-VALUES	('radagon', 'radagon@goldenorder.gov', '$2a$16$gK9/K4lesb/fbkX59cAYvOwj86/QY7vxoX104UQODbl5ok4CoT8yW', 'ADMIN'), --'praise2fingers'
-		('mArgIt', 'king_morgott@mail.leyndell.org', '$2a$16$jujbcHLfoP0GqBYNAJLEp.Lmz9Op35u.AbQMxtHzp5V8qSdEvwrDK', 'STAFF'), --'foo1ish-ambition'
-		('m00n_queen', 'rennala.caria@raya-lucaria.edu', '$2a$16$pfedYo38SnM3AbInlg4ZkuuPg0aBJ.xMXZbJF6O6Eoju.Ube97Wfu', 'STAFF'), --'r4d4g0nSUX'
-		('goldenboi96', 'godrickRULZ1996@storm-mail.org', '$2a$16$Y7OI8MXXj3R5IR3am0nBkunKTOr54OB4WLquSNusUR3tClbXgjzbG', 'STAFF'), --'i_got_99_arms'
-		('teh_all_knowing', 'gideon_o@erdmail.com', '$2a$16$BsXx8BCuXlDV1Wk2AC5WzuYxuGHM.UqdQ5muWz9OFWI7kynCB42CS', 'USER'), --'TRUE_3LD3N_L0RD'
-		('daughterOfTheCosmos', 'sellen@raya-lucaria.edu', '$2a$16$CqPVOkVVlcSD3n.K6tFguO67Xokhy9bxAYahc9pRMEvJpDSn3VQki', 'USER'), --'arcaneDame456'
-		('xX_alexander_Xx', 'iron_fist_alex@erdmail.com', '$2a$16$yL.qlniID16Z/dPeoD1kcOvUjh3uNb/XkpzYHgiaAN9ftWtc58Pbq', 'USER'), --'1234qwer'
-		('malenia-blade-of-miquella', 'malenia-blade-of-miquella@haligmail.org', '$2a$16$2AWQ01CmUqCXYm62kZ4LsO86yHo4f1sy2hsiV5JxOcGuTkb.qOyY2', 'USER'), --'1337W-0L'
-		('trustypatches', 'kenneth.haight@erdmail.com', '$2a$16$EzHLCmaxuN/ltzlMBVI45uJUXWNWF3O.BmFDqPfGfEmM11uaYHlXS', 'STAFF'); --'O0O0OOO0'
+VALUES	('radagon', 'radagon@goldenorder.gov', '$2a$16$gK9/K4lesb/fbkX59cAYvOwj86/QY7vxoX104UQODbl5ok4CoT8yW', 'ADMIN'),
+		('mArgIt', 'king_morgott@mail.leyndell.org', '$2a$16$jujbcHLfoP0GqBYNAJLEp.Lmz9Op35u.AbQMxtHzp5V8qSdEvwrDK', 'STAFF'),
+		('m00n_queen', 'rennala.caria@raya-lucaria.edu', '$2a$16$pfedYo38SnM3AbInlg4ZkuuPg0aBJ.xMXZbJF6O6Eoju.Ube97Wfu', 'STAFF'),
+		('goldenboi96', 'godrickRULZ1996@storm-mail.org', '$2a$16$Y7OI8MXXj3R5IR3am0nBkunKTOr54OB4WLquSNusUR3tClbXgjzbG', 'STAFF'),
+		('teh_all_knowing', 'gideon_o@erdmail.com', '$2a$16$BsXx8BCuXlDV1Wk2AC5WzuYxuGHM.UqdQ5muWz9OFWI7kynCB42CS', 'USER'),
+		('daughterOfTheCosmos', 'sellen@raya-lucaria.edu', '$2a$16$CqPVOkVVlcSD3n.K6tFguO67Xokhy9bxAYahc9pRMEvJpDSn3VQki', 'USER'),
+		('xX_alexander_Xx', 'iron_fist_alex@erdmail.com', '$2a$16$yL.qlniID16Z/dPeoD1kcOvUjh3uNb/XkpzYHgiaAN9ftWtc58Pbq', 'USER'),
+		('malenia-blade-of-miquella', 'malenia-blade-of-miquella@haligmail.org', '$2a$16$2AWQ01CmUqCXYm62kZ4LsO86yHo4f1sy2hsiV5JxOcGuTkb.qOyY2', 'USER'),
+		('trustypatches', 'kenneth.haight@erdmail.com', '$2a$16$EzHLCmaxuN/ltzlMBVI45uJUXWNWF3O.BmFDqPfGfEmM11uaYHlXS', 'STAFF');
 
 
 
